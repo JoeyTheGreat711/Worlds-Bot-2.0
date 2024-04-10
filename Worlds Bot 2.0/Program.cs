9126495ce73161756a3f2d8a14ac79f3a3a228e2
Joey Chastain <joey.e.chastain@gmail.com>
2024-04-10 14:30:56 -0400

    working

--- Worlds Bot 2.0/Program.cs
+++ Worlds Bot 2.0/Program.cs
@@ -3,14 +3,6 @@
 
 using Discord;
 using Discord.WebSocket;
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using System.Linq;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.IO;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 
@@ -19,12 +11,12 @@ namespace Worlds_Bot_2._0
     class Program
     {
         //important discord ids
-        ulong masterUserId = 637997156883628046;
-        ulong errorDmChannel = 1100666304463126589;
-        ulong selfId = 1100380213868777482;
+        static ulong masterUserId = 637997156883628046;
+        static ulong errorDmChannel = 1100666304463126589;
+        static ulong selfId = 1100380213868777482;
 
         static int eventID = 0;// last worlds: 49726 //kalahari = 48542 open = 48473 2022 = 45258 2023 = 49725 ms = 49726
-        static string url = "https://www.robotevents.com/api/v2/events/" + eventID;
+        static string url = "https://www.robotevents.com/api/v2/events";
         private static DiscordSocketClient discordClient;
         static List<List<string>> allTeams = new List<List<string>>();
         static List<string> divisions = new List<string>();
@@ -42,8 +34,13 @@ namespace Worlds_Bot_2._0
             roboteventsClient.DefaultRequestHeaders.Add("user-agent", "C# Discord Bot");
             roboteventsClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", secrets.robotEventsToken);
 
+            eventID = int.Parse("" + JsonConvert.DeserializeObject<dynamic>((await (await roboteventsClient.GetAsync(url + "?sku=" + eventSku)).Content.ReadAsStringAsync())).data[0].id);
+            Console.WriteLine("found event id: " + eventID);
+            url += "/" + eventID;
             Console.WriteLine("requesting teams...");
-            List<int> divs = JsonConvert.DeserializeObject<Event>(await (await roboteventsClient.GetAsync(url)).Content.ReadAsStringAsync()).divisions.Select(d => d.id).Where(id => id < 100).ToList();
+            HttpResponseMessage response = await roboteventsClient.GetAsync(url);
+            string json = await response.Content.ReadAsStringAsync();
+            List<int> divs = JsonConvert.DeserializeObject<Event>(json).divisions.Select(d => d.id).Where(id => id < 100).ToList();
             foreach (int i in divs)
             {
                 allTeams.Add(new List<string>());
@@ -52,9 +49,9 @@ namespace Worlds_Bot_2._0
                 {
                     Console.WriteLine("requesting div " + i + " page " + page);
                     string target = url + "/divisions/" + i + "/rankings?page=" + page;
-                    HttpResponseMessage response = await roboteventsClient.GetAsync(target);
+                    HttpResponseMessage pageResponse = await roboteventsClient.GetAsync(target);
                     page++;
-                    Division data = JsonConvert.DeserializeObject<Division>(await response.Content.ReadAsStringAsync());
+                    Division data = JsonConvert.DeserializeObject<Division>(await pageResponse.Content.ReadAsStringAsync());
                     if (page > data.meta.last_page)
                         page = 0;
                     allTeams[allTeams.Count - 1].AddRange(data.data.Select(t => t.team.name));
@@ -70,17 +67,17 @@ namespace Worlds_Bot_2._0
             discordClient = new DiscordSocketClient(config);
             discordClient.Log += Log;
             discordClient.MessageReceived += MessageRecieved;
-            await discordClient.LoginAsync(TokenType.Bot, discordToken);
+            await discordClient.LoginAsync(TokenType.Bot, secrets.discordToken);
             await discordClient.StartAsync();
             await Task.Delay(-1);
 
         }
-        private Task Log(LogMessage msg)
+        private static Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
-        private async Task MessageRecieved(SocketMessage messageData)
+        private static async Task MessageRecieved(SocketMessage messageData)
         {
             string mostRecentRequest = "";
             string mostRecentResponse = "";
@@ -114,16 +111,24 @@ namespace Worlds_Bot_2._0
                                 page = 0;
                         }
                         string reply = "";
-                        int W = 0, L = 0, T = 0;
+                        int W = 0, L = 0, T = 0, unplayed = 0;
+                        Console.WriteLine(curTeam);
                         foreach (Match m in matches)
                         {
                             int teamScore = m.alliances.Find(a => a.teams.Exists(t => t.team.name == curTeam)).score;
                             int oppScore = m.alliances.Find(a => !a.teams.Exists(t => t.team.name == curTeam)).score;
-                            if (!String.IsNullOrEmpty(m.started) && m.name.ToLower().Contains("qualifier"))
+                            if (m.name.ToLower().Contains("qualifier"))
                             {
-                                if (teamScore > oppScore) W++;
-                                if (teamScore < oppScore) L++;
-                                if (teamScore == oppScore) T++;
+                                if (teamScore + oppScore > 0)
+                                {
+                                    if (teamScore > oppScore) W++;
+                                    if (teamScore < oppScore) L++;
+                                    if (teamScore == oppScore) T++;
+                                    T += unplayed;
+                                    unplayed = 0;
+                                }
+                                else
+                                    unplayed++;
                             }
                             string name = m.name;
                             if (name.ToLower().Contains("qualifier"))
